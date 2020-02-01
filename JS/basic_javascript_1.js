2e66b54850f11acf3df0ad3231b63fd60ee386e1
Pierson Han <piersonhan@gmail.com>
2020-01-31 17:11:53 -0800

    Basic Javascript projects

---
+++ JS/basic_javascript_1.js
@@ -0,0 +1,30 @@
+alert('Hello, Fuckers'); /*this and the one below are different ways to "popup text"*/
+window.alert('Hello ASSHOLES');
+
+document.write("Pierson said, \"This is how this works"
++" and now the sentence will continue onwards"    /*add concatenate to the string*/
++", also something just beeped in my house that I"
++" never heard before.\" so thats neat.");
+
+
+document.write('Hello losers');/*this is used to write text, duh.*/
+
+var P = " this is a string and I am saying,\"this is a how i would use"
++ " a \' and\" or \\ its very odd.\""
++" but I am learning javascript!";/*learning to assign a Variable.*/
+
+document.write(P);
+
+var PI = "this is an assignment that required me to figure it out on my own";
+window.alert(PI); /*also pierson, keep in mind the spaces. sometime it acts weird.*/
+
+var members = "Pierson, Fy, and Bamboo", dude = "Pierson", they = "Fy", fatty = "Bamboo"; 
+/*this is how i would assign more than one variable at one time but it feels sloppy to me*/
+var dude = dude.fontcolor("yellow");
+var they = they.fontcolor("purple");/*color font*/
+var fatty = fatty.fontcolor("red");
+var members = members.fontcolor("green"); 
+
+document.write(members);
+
+
