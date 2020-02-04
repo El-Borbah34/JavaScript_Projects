c797e58302b71e70fd4e74472721d8af0853ff93
Pierson Han <piersonhan@gmail.com>
2020-02-04 12:46:31 -0800

    add javascript 9 and basic css 1

---
+++ JS/basic_javascript_9.js
@@ -0,0 +1,45 @@
+/*===========ALL JAVASCRIPT IVE LEARNED================*/
+function countdown()  {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds--; // seconds = seconds - 1;
+        document.getElementById("timer").innerHTML = seconds;
+        if (seconds > 0)
+        {
+            setTimeout(tick, 1000);
+        }
+        else {
+            alert("Time's Up!");
+        }
+    }
+    tick();
+}
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n)  {
+    showSlides(slideIndex +=n);
+}
+
+function currentSlide(n)    {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n)  {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n >slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i <dots.length; i++)    {
+        dots[i].className = dots[i].className.replace(" active","");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots [slideIndex-1].className += " active"
+}
